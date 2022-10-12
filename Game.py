d0ab40b2bfc0f010a75420471c8847e2e55ce833
kyliemckinley01 <kmckinley@potomacschool.org>
2022-10-12 08:55:41 -0400

    hi

--- Game.py
+++ Game.py
@@ -1,11 +1,11 @@
-import gameboard from gameboard
+import Gameboard from gameboard
 gameboard = Gameboard()
 def Game(P1, P2):
     while (gameboard.isWinner()==false): #alternate players making a move, if there is a winner after each move exit while loop and find winner
         P1.move()
-        P1.play(P1,P1.move(),getValue())
+        P1.play(P1,P1.move(),gameboard.getValue())
         if(gameboard.isWinner()==true):
             break
         P2.move()
-        P2.play(P2,P2.move(),getValue())
-    findWinner()
+        P2.play(P2,P2.move(),gameboard.getValue())
+    gameboard.findWinner()
