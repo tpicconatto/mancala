6df8ea79dfa7bb37455b0ceef16a6bee523c4b57
kyliemckinley01 <kmckinley@potomacschool.org>
2022-10-18 08:37:26 -0400

    hi

--- Game.py
+++ Game.py
@@ -8,6 +8,7 @@ player2 = Human(2)
 def Game(P1, P2):
     while (gameboard.isWinner()== False): #alternate players making a move, if there is a winner after each move exit while loop and find winner
         print(gameboard)
+        print ("Player 1")
         index = P1.move()
         num = gameboard.play(P1,index,gameboard.getValue(0,index))
         while num ==-1:
